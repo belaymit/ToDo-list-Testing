cda36110d34bf59d40febc0bbf3bb569b18db94a
Glenda Diaz <glendadiazah@outlook.com>
2022-12-14 15:47:39 -0600

    Add test for removeTask function

--- toDoList.test.js
+++ toDoList.test.js
@@ -26,6 +26,31 @@ describe('add and remove', () => {
   });
 
   test('remove to do', () => {
+    const todoList = [
+      {
+        description: 'something 1',
+      completed: false,
+      index: 0,
+      },
+      {
+        description: 'something 2',
+      completed: false,
+      index: 1,
+      },
+    ];
+    const i =0;
+    const output = removeToDO(i,todoList);
+    expect(output).toEqual([{description: 'something 2', completed: false, index: 1}]);
 
+    localStorage.setItem(todoList).JSON.stringify(output);
+    expect(JSON.parse(localStorage.getItem(todoList))).toEqual(output);
+
+    for (let i = 0; i < todoList.length; i += 1) {
+      document.body.innerHTML = '<div class = "list-item">'
+      + '  <ul id="list"><li></li></ul>'
+      + '</div>';
+    }
+    const listItem = document.querySelectorAll('.list-item');
+    expect(listItem).toHaveLength(output.length);
+  })
   });
-});
