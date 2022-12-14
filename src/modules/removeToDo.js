e3b9202aadef0ee629ea3545dec650152abf874f
belaymit <2belamit@gmail.com>
2022-12-15 01:13:40 +0300

    removed unnecessary lines of codes and comments

--- src/modules/removeToDo.js
+++ src/modules/removeToDo.js
@@ -1,10 +1,5 @@
 function deleteAnItem(i, toDoList) {
-  const temp = [...toDoList];
   toDoList.splice(i, 1);
-
-  if (toDoList === temp) {
-    throw new Error('Error deleting item');
-  }
   return toDoList;
 }
 
